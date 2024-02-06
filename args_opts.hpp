d1573f9d10ed59e2eb2fd88597d2c0073542a1ba
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-02-06 22:38:34 +0200

    Modified ICF and OFC parameters to get in the color fomats

--- args_opts.hpp
+++ args_opts.hpp
@@ -14,11 +14,13 @@
 
 
 #define Ver "1.0.0"
-#define CLIdebug
+//#define CLIdebug
 #define DevMode
 
 std::string inFile; //For input color array from file
 std::string outFile; //For exporting the converted color array into a file
+std::string colorFMTIn;
+std::string colorFMTout;
 
 
 
@@ -29,21 +31,46 @@ auto CLI_opt = (
     clipp::option("-v", "--version")([]{std::cout << "Version: " << Ver << "\n";}),
     clipp::option("-sl", "--show_license")([]{std::cout << Licenses;}),
     clipp::option("-scf", "--show_color_fmts")([]{std::cout << ColorFMTs;}),
-    clipp::option("-icf", "--in_color_fmt")([]{
+    clipp::option("-icf:", "--in_color_fmt:") & clipp::value("in color fmt", colorFMTIn)([]{
         #ifdef CLIdebug
             std::cout << "[In Color fmt]\n";
         #endif
+        if(colorFMTIn.find("RGB") != std::string::npos)
+            std::cout << "Input Color Format: RGB\n";
+        else if(colorFMTIn.find("HSL") != std::string::npos)
+            std::cout << "Input Color Format: HSL\n";
+        else if(colorFMTIn.find("HEX") != std::string::npos)
+            std::cout << "Input Color Format: HEX\n";
+        //Failed to return error when string is empty
+        //else if(!colorFMTIn.size())
+            //Err("You must enter a input color format!");
+            //Err("Unknown input color format!");
+        else
+        {
+            Err("Unknown input color format!");
+            std::cout << "'" << colorFMTIn << "'\n";
+        }
     }),
-    clipp::option("-ocf", "--out_color_fmt")([]{
+    clipp::option("-ocf:", "--out_color_fmt:") & clipp::value("out color format", colorFMTout)([]{
         #ifdef CLIdebug
             std::cout << "[Output Color Fmt]\n";
         #endif
+        if(colorFMTout.find("RGB") != std::string::npos)
+            std::cout << "OCF: RGB\n";
+        else if(colorFMTout.find("HSL") != std::string::npos)
+            std::cout << "OCF: HSL\n";
+        else if(colorFMTout.find("HEX") != std::string::npos)
+            std::cout << "OCF: HEX\n";
+        else
+        {
+            Err("Unknown output color format!");
+            std::cout << "'" << colorFMTout << "'\n";
+        }
     }),
     clipp::option("-fi", "--file_in") & clipp::value("input file", inFile)([]{
         #ifdef CLIdebug
             std::cout << "[File Input]\n";
         #endif
-        std::cout << "Color array input from file: " << inFile << "\n";
         ReadInputFile(inFile);
     }),
     clipp::option("-fo", "--file_out") & clipp::value("out file", outFile)([]{
