abd3362840585fb9c400cd343b2924fc88290a48
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-02-11 20:36:05 +0200

    marked what works and what's not working

--- core/color_defs.h
+++ core/color_defs.h
@@ -11,31 +11,31 @@ typedef struct
     unsigned char R;
     unsigned char G;
     unsigned char B;
-}RGB;
+} RGB;
 
 typedef struct 
 {
     char R[3];
     char G[3];
     char B[3];
-}RGB_Hex;
+} RGB_Hex;
 
 typedef struct
 {
     int H;
     double S;
     double L;
-}HSL;
+} HSL;
 
 bool checkRGB_HEX(RGB_Hex rgbh);
 bool checkHSL(HSL hsl);
 
-void HEX2RGB(RGB_Hex rgbh, RGB *rgb);
-void HEX2HSL(RGB_Hex rgbh, HSL *hsl);
-void RGB2HEX(RGB rgb, RGB_Hex *rgbh);
-void RGB2HSL(RGB rgb, HSL *hsl);
-void HSL2RGB(HSL hsl, RGB *rgb);
-void HSL2HEX(HSL hsl, RGB_Hex *rgbh);
+void HEX2RGB(RGB_Hex rgbh, RGB *rgb); //Success
+void HEX2HSL(RGB_Hex rgbh, HSL *hsl); //FAILED
+void RGB2HEX(RGB rgb, RGB_Hex *rgbh); //Success
+void RGB2HSL(RGB rgb, HSL *hsl);      //FAILED
+void HSL2RGB(HSL hsl, RGB *rgb);      //FAILED
+void HSL2HEX(HSL hsl, RGB_Hex *rgbh); //Forgot to implement
 void printRGB(RGB rgb);
 void printRGBH(RGB_Hex rgbh);
 void printHSL(HSL hsl);
