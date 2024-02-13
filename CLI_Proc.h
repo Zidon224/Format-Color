551f4068ccf5e8ed4e5e756034d2a1770c91d635
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-02-13 22:30:27 +0200

    Questioning my choices LMFAO

--- CLI_Proc.h
+++ CLI_Proc.h
@@ -4,11 +4,17 @@
 #include <iostream>
 #include <string>
 
+#include "args_opts.hpp"
+#include "core/convert_utils.h"
+
 
 
 //These splitting algorithms will be used with the RGB and HSL color input.
 
 #define MAX_STR_LEN 3
+
+HSL procHSL;
+RGB_Hex rgbhInput;
 
 
 std::string ICF_str[MAX_STR_LEN];
@@ -38,5 +44,43 @@ static void stringSplit(std::string inpStr, char separator)
             startInxed = endIndex + 1;
         }
         i++;
+    }
+}
+
+static void CheckInOut(std::string inParam, std::string outParam)
+{
+    //RGB -> HEX
+    if(inParam == "RGB=" && outParam == "HEX")
+    {
+        std::cout << "Check icf: RGB -> HEX\n";
+    }
+    //RGB -> HSL
+    else if(inParam == "RGB=" && outParam == "HSL")
+    {
+        std::cout << "Check ICF OCF: RGB -> HSL\n";
+    }
+    //HSL -> RGB
+    else if(inParam == "HSL=" && outParam == "RGB")
+    {
+        std::cout << "Check ICF OCF: HSL -> RGB\n";
+    }
+    //HSL -> HEX
+    else if(inParam == "HSL=" && outParam == "HEX")
+    {
+        std::cout << "Check ICF OCF: HSL -> HEX\n";
+    }
+    //HEX -> HSL
+    else if(inParam == "HEX=" && outParam == "HSL")
+    {
+        std::cout << "Check ICF OCF: HEX -> HSL\n";
+        //rgbhInput = inColor;
+        //HEX2HSL(rgbhInput, &procHSL);
+        //Defenetly WRONG!!!
+        //Need to find an actual way to do this at first...
+    }
+    //HEX -> RGB
+    else if(inParam == "HEX=" && outParam == "RGB")
+    {
+        std::cout << "Check ICF OCF: HEX -> RGB\n";
     }
 }
