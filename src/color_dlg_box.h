bc61ed7e2d8df9a1571b7be7b31f14d2939a2998
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-05-09 20:01:09 +0300

    for -ncpk command (Windows implementation only)

---
+++ src/color_dlg_box.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdio.h>
+
+#if defined _WIN32 || _WIN64
+  #include <windows.h>
+#endif
+
+static void NativeDlgColorBox()
+{
+  #if defined _WIN32 || _WIN64
+    CHOOSECOLOR cc;                 // common dialog box structure 
+    static COLORREF acrCustClr[16]; // array of custom colors 
+    HBRUSH hbrush;                  // brush handle
+    static DWORD rgbCurrent;        // initial color selection
+
+    // Initialize CHOOSECOLOR 
+    ZeroMemory(&cc, sizeof(cc));
+    cc.lStructSize = sizeof(cc);
+    cc.hwndOwner = NULL;            //No need
+    cc.lpCustColors = (LPDWORD) acrCustClr;
+    cc.rgbResult = rgbCurrent;
+    cc.Flags = CC_FULLOPEN | CC_RGBINIT | CC_SOLIDCOLOR | CC_ANYCOLOR;
+ 
+    if (ChooseColor(&cc))
+    {
+      hbrush = CreateSolidBrush(cc.rgbResult);
+      rgbCurrent = cc.rgbResult; 
+    }
+  #endif
+}
