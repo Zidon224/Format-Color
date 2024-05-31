f0b093abd2d713a0894d5eb2e8f05d7e0e074e41
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-05-31 14:02:11 -0700

    I finally got things to work REAL!!!

--- src/color_dlg_box.h
+++ src/color_dlg_box.h
@@ -1,18 +1,31 @@
+/*
+Function and definitions for native color selectors
+*/
+
+
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #if defined _WIN32 || _WIN64
   #include <windows.h>
+  #include <Intsafe.h>
+  #include <wingdi.h>
 #endif
+
+uint8_t Rv;
+uint8_t Gv;
+uint8_t Bv;
 
 static void NativeDlgColorBox()
 {
   #if defined _WIN32 || _WIN64
-    CHOOSECOLOR cc;                 // common dialog box structure 
-    static COLORREF acrCustClr[16]; // array of custom colors 
+    CHOOSECOLOR cc;                 // common dialog box structure
+    static COLORREF acrCustClr[16]; // array of custom colors
     HBRUSH hbrush;                  // brush handle
-    static DWORD rgbCurrent;        // initial color selection
+    COLORREF rgbCurrent;            // initial color selection
+    
 
     // Initialize CHOOSECOLOR 
     ZeroMemory(&cc, sizeof(cc));
@@ -25,7 +38,14 @@ static void NativeDlgColorBox()
     if (ChooseColor(&cc))
     {
       hbrush = CreateSolidBrush(cc.rgbResult);
-      rgbCurrent = cc.rgbResult; 
+      rgbCurrent = cc.rgbResult;
+      //Thank you Ex-ce-pt(github) for help!
+      Rv = (uint8_t) (GetRValue(rgbCurrent));
+      Gv = (uint8_t) (GetGValue(rgbCurrent));
+      Bv = (uint8_t) (GetBValue(rgbCurrent));
+      #ifdef COLOR_SELECTOR_DEBUG
+        printf("[Debug] - R: %d | G: %d | B: %d\n", Rv, Gv, Bv);
+      #endif
     }
   #endif
   #ifdef __linux__
