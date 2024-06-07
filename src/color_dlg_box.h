66625eee9c84dd53dec184e4c2f4980db0cfc313
Zidon224 <92180085+Zidon224@users.noreply.github.com>
2024-06-07 20:24:11 +0300

    I got things to work once again!!!

--- src/color_dlg_box.h
+++ src/color_dlg_box.h
@@ -23,6 +23,15 @@ uint8_t Gv;
 uint8_t Bv;
 //uint8_t Ro, Go, Bo;
 
+const char * FN_Global;
+
+//My little helper that gets the file name from CLI LMfAO
+static void GetFilanameFromCLI(const char * FN)
+{
+  FN_Global = FN;
+}
+
+//Do NOT change!!
 static const int DefaultCustomColors[16][3] =
 {
   255, 0, 0,
@@ -82,27 +91,41 @@ static void NativeDlgColorBox(bool DefCol, bool wca)
       Rv = (uint8_t) (GetRValue(rgbCurrent));
       Gv = (uint8_t) (GetGValue(rgbCurrent));
       Bv = (uint8_t) (GetBValue(rgbCurrent));
-    //uint8_t Ro, Go, Bo;
+      uint8_t Ro, Go, Bo;
     
-    /*if(wca)
-    {
-      Write_Color_Array("hello.ini", 16, acrCustClr);
-    }*/
-      
-      /*for(int i = 0; i < 16; i++)
+      if(wca)
       {
-        //Read RGB values from each of the 16 colors
-        Ro = (uint8_t) (GetRValue(acrCustClr[i]));
-        Go = (uint8_t) (GetGValue(acrCustClr[i]));
-        Bo = (uint8_t) (GetBValue(acrCustClr[i]));
-        #ifdef COLOR_SELECTOR_DEBUG
-          printf("Color array: Counter: %d R= %d | G= %d | B= %d\n", i, Ro, Go, Bo);
-        #endif
-        //Assign contents to the file
-        fprintf(ColrArr, "[Color%d]\nR=%d\nG=%d\nB=%d\n\n", i, Ro, Go, Bo);
-      }*/
+        char tempR[10];
+        char tempG[10];
+        char tempB[10];
+        char temp[50];
+        ini_table_s* OutputColorArray = ini_table_create();
+
+        for(int i = 0; i < 16; i++)
+        {
+          //Read RGB values from each of the 16 colors
+          Ro = (uint8_t) (GetRValue(acrCustClr[i]));
+          Go = (uint8_t) (GetGValue(acrCustClr[i]));
+          Bo = (uint8_t) (GetBValue(acrCustClr[i]));
   
-      //fclose(ColrArr); //Close file after writing it's contents
+          itoa(Ro, tempR, 10);
+          itoa(Go, tempG, 10);
+          itoa(Bo, tempB, 10);
+  
+          //Define each of the custom color with a nuber from 0 to 16 as a string
+          sprintf(temp, "RGBColor_%d", i);
+  
+          //Assign contents to the file
+          ini_table_create_entry(OutputColorArray, temp, "R", tempR);
+          ini_table_create_entry(OutputColorArray, temp, "G", tempG);
+          ini_table_create_entry(OutputColorArray, temp, "B", tempB);
+
+          #ifdef COLOR_SELECTOR_DEBUG
+            printf("Color array: Counter: %d R= %d | G= %d | B= %d\n", i, Ro, Go, Bo);
+          #endif
+        }
+        ini_table_write_to_file(OutputColorArray, FN_Global);
+      }
     }
   #endif
   #ifdef __linux__
